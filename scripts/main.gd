90b1c5a0acabf15e598566f99c4130392bf5d8f6
KyoheiAbc <autumn8november@gmail.com>
2026-09-10 11:48:36 +0900

    Enhance rival follower behavior and adjust visualization in main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -44,12 +44,12 @@ func _ready() -> void:
 	pl_mesh.modulate = Color(0.2, 0.2, 0.9)
 
 
-	for i in range(3):
+	for i in range(5):
 		var rival_follower = PathFollow2D.new()
 		rival_follower.loop = true
 		path.add_child(rival_follower)
 		rival_followers.append(rival_follower)
-		rival_follower.v_offset = randf_range(-8.0, 8.0)
+		rival_follower.v_offset = randf_range(-16.0, 16.0)
 
 		var rival = Node2D.new()
 		add_child(rival)
@@ -59,7 +59,7 @@ func _ready() -> void:
 		rival.add_child(rival_mesh)
 		rival_mesh.mesh = QuadMesh.new()
 		rival_mesh.mesh.size = Vector2(12, 24)
-		rival_mesh.modulate = Color.from_hsv(i / 3.0, 1.0, 1.0)
+		rival_mesh.modulate = Color.from_hsv(i / 6.0, 1.0, 1.0)
 
 		rival_follower.progress_ratio = 0
 		rival.position = rival_follower.position
@@ -69,7 +69,7 @@ func _ready() -> void:
 		rival_follower.add_child(follower_mesh)
 		follower_mesh.mesh = QuadMesh.new()
 		follower_mesh.mesh.size = Vector2(12, 12)
-		follower_mesh.modulate = Color.from_hsv(i / 3.0, 0.5, 1.0)
+		follower_mesh.modulate = Color.from_hsv(i / 5.0, 0.5, 1.0)
 		follower_mesh.z_index = 64
 
 
@@ -109,9 +109,14 @@ func _process(delta: float) -> void:
 		else:
 			pass
 
-		var diff = follower.position - rival.position
-		rival.position += diff.normalized()
-		rival.rotation = diff.angle() + PI / 2
+		var current_direction = Vector2.UP.rotated(rival.rotation)
+		var target_direction = (follower.position - rival.position).normalized()
+		var angle_diff = current_direction.angle_to(target_direction)
+		if angle_diff > 0.1:
+			rival.rotation += 0.05
+		elif angle_diff < -0.1:
+			rival.rotation -= 0.05
+		rival.position += Vector2.UP.rotated(rival.rotation) * randf_range(0.8, 1.1)
 
 
 func set_point_in_out(curve: Curve2D, index: int, point: Vector2):
