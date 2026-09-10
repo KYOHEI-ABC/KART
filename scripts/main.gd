8cf51c93f7df4e785c89d89c55df6c1f93dde8d9
KyoheiAbc <autumn8november@gmail.com>
2026-09-10 10:58:06 +0900

    Refactor path initialization and player/rival setup in main.gd

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,11 +3,17 @@ extends Node
 
 var path: Path2D
 var path_follow: PathFollow2D
+
 var player: Node2D
+var rival: Node2D
 
 func _ready() -> void:
 	var camera = Camera2D.new()
 	add_child(camera)
+
+	path = Path2D.new()
+	add_child(path)
+	path.curve = Curve2D.new()
 
 	var points: Array[Vector2] = [
 		Vector2(0, 0),
@@ -17,15 +23,30 @@ func _ready() -> void:
 		Vector2(100, 200),
 	]
 
-	path = Path2D.new()
-	path.curve = Curve2D.new()
-
 	for point in points:
 		path.curve.add_point(point)
-
 	path.curve.add_point(points[0])
 
-	add_child(path)
+	path_follow = PathFollow2D.new()
+	path_follow.loop = true
+	path.add_child(path_follow)
+
+	player = Node2D.new()
+	add_child(player)
+	var pl_mesh = MeshInstance2D.new()
+	player.add_child(pl_mesh)
+	pl_mesh.mesh = QuadMesh.new()
+	pl_mesh.mesh.size = Vector2(12, 24)
+	pl_mesh.modulate = Color(0.2, 0.2, 0.9)
+
+	rival = Node2D.new()
+	add_child(rival)
+	var rival_mesh = MeshInstance2D.new()
+	rival.add_child(rival_mesh)
+	rival_mesh.mesh = QuadMesh.new()
+	rival_mesh.mesh.size = Vector2(12, 24)
+	rival_mesh.modulate = Color(0.9, 0.2, 0.2)
+
 
 	var baked_points = path.curve.get_baked_points()
 	var line_road = Line2D.new()
@@ -35,35 +56,12 @@ func _ready() -> void:
 	add_child(line_road)
 	line_road.z_index = -1
 
-	path_follow = PathFollow2D.new()
-	path_follow.loop = true
-	path_follow.rotates = true
-	path.add_child(path_follow)
-
-	var follower_mesh = MeshInstance2D.new()
-	var box_mesh = QuadMesh.new()
-	box_mesh.size = Vector2(12, 24)
-	follower_mesh.mesh = box_mesh
-	follower_mesh.modulate = Color(0.9, 0.2, 0.2)
-
-	follower_mesh.rotation = PI / 2
-
-	path_follow.add_child(follower_mesh)
-
-	player = Node2D.new()
-	var pl_mesh = MeshInstance2D.new()
-	var pl_box_mesh = QuadMesh.new()
-	pl_box_mesh.size = Vector2(12, 24)
-	pl_mesh.mesh = pl_box_mesh
-	pl_mesh.modulate = Color(0.2, 0.2, 0.9)
-	player.add_child(pl_mesh)
-	add_child(player)
 
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_A):
-		player.rotation -= delta
+		player.rotation -= 0.1
 	if Input.is_key_pressed(KEY_D):
-		player.rotation += delta
+		player.rotation += 0.1
 	var direction = Vector2.UP.rotated(player.rotation)
 	player.position += direction
 
@@ -77,3 +75,7 @@ func _process(delta: float) -> void:
 
 	if path_follow:
 		path_follow.progress += 1
+
+	var rival_direction = path_follow.position - rival.position
+	rival.position = path_follow.position
+	rival.rotation = rival_direction.angle() + PI / 2
